ac311b0a409e556c1b75dc5e407cfb38ec64478c
daiki.ishikawa <diking@gmail.com>
2018-06-29 05:58:52 +0000

    index test

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-        //anna
-        <div>
-            <a href="{{route ('members.anna')}}"><img src="annie.jpg" alt="アンナ">anna</a>
-        </div>
+   <p>daiki</p>
+   
+    <!--    
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.anna')}}"><img src="annie.jpg" alt="アンナ">anna</a>-->
+    <!--    </div>-->
        
-       //kanna
-        <div>
-            <a href="{{route ('members.kanna')}}"><img src="kanna.jpg" alt="カンナ">kanna</a>
-        </div>
+    <!--   //kanna-->
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.kanna')}}"><img src="kanna.jpg" alt="カンナ">kanna</a>-->
+    <!--    </div>-->
        
-       //shingo
-        <div>
-            <a href="{{route ('members.shingo')}}"><img src="shingo.jpg" alt="シンゴ">shingo</a>
-        </div>
+    <!--   //shingo-->
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.shingo')}}"><img src="shingo.jpg" alt="シンゴ">shingo</a>-->
+    <!--    </div>-->
        
-       //shungo
-        <div>
-            <a href="{{route ('members.shungo')}}"><img src="shungo.jpg" alt="シュンゴ">shungo</a>
-        </div>
+    <!--   //shungo-->
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.shungo')}}"><img src="shungo.jpg" alt="シュンゴ">shungo</a>-->
+    <!--    </div>-->
        
-       //daiki
-        <div>
-            <a href="{{route ('members.daiki')}}"><img src="daiki.jpg" alt="ダイキ">daiki</a>
-        </div>
+    <!--   //daiki-->
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.daiki')}}"><img src="daiki.jpg" alt="ダイキ">daiki</a>-->
+    <!--    </div>-->
        
-       //hisashi
-        <div>
-            <a href="{{route ('members.hisashi')}}"><img src="hisashi.jpg" alt="ヒサシ">hisashi</a>
-        </div>
-    </div>    
+    <!--   //hisashi-->
+    <!--    <div>-->
+    <!--        <a href="{{route ('members.hisashi')}}"><img src="hisashi.jpg" alt="ヒサシ">hisashi</a>-->
+    <!--    </div>-->
+    <!--</div>    -->
 @endsection
