6079d4ee133297b943e20ae38415ff24154da4c5
kanna.hirao <kanna.hirao@rakuten.com>
2018-06-29 06:18:55 +0000

    welcome ifelse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        @include('members.index')
+    @else
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>Koders</h1>
-                @if (!Auth::check())
                     <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">member紹介ページにクリック</a>
-                @endif
-                
             </div>
         </div>
     </div>
+    @endif
 @endsection
 
    
