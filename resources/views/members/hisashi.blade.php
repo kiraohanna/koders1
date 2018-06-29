e0dc3cf1375c925bf78978fd59079604d37e1249
EC2 Default User <ec2-user@ip-172-31-47-27.us-east-2.compute.internal>
2018-06-29 05:45:58 +0000

    simple personal page

---
+++ resources/views/members/hisashi.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Hisashi のページ<h1>
+    
+    <img src=pictures.hisashi.jpg>
+@endsection
---
+++ resources/views/members/shungo.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Shungo のページ<h1>
+    
+    <img src=pictures.shungo.jpg>
+@endsection
