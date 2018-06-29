4e39a52f2a4fca0e94ed38cb93dc92271163f00e
daiki.ishikawa <diking@gmail.com>
2018-06-29 06:28:08 +0000

    index add picture folder

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -3,34 +3,34 @@
 @section('content')
    <p>daiki</p>
    
-    <!--    
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.anna')}}"><img src="annie.jpg" alt="アンナ">anna</a>-->
-    <!--    </div>-->
+        
+        <div>
+            <a href="{!!link_to_route ('members.anna')}"><img src="pictures/annie.jpg" alt="アンナ">anna</a>
+        </div>
        
-    <!--   //kanna-->
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.kanna')}}"><img src="kanna.jpg" alt="カンナ">kanna</a>-->
-    <!--    </div>-->
+       //kanna
+        <div>
+            <a href="{!!link_to_route ('members.kanna')}"><img src="pictures/kanna.jpg" alt="カンナ">kanna</a>
+        </div>
        
-    <!--   //shingo-->
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.shingo')}}"><img src="shingo.jpg" alt="シンゴ">shingo</a>-->
-    <!--    </div>-->
+       //shingo
+        <div>
+            <a href="{!!link_to_route ('members.shingo')}"><img src="pictures/shingo.jpg" alt="シンゴ">shingo</a>
+        </div>
        
-    <!--   //shungo-->
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.shungo')}}"><img src="shungo.jpg" alt="シュンゴ">shungo</a>-->
-    <!--    </div>-->
+       //shungo
+        <div>
+            <a href="{!!link_to_route ('members.shungo')}"><img src="pictures/shungo.jpg" alt="シュンゴ">shungo</a>
+        </div>
        
-    <!--   //daiki-->
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.daiki')}}"><img src="daiki.jpg" alt="ダイキ">daiki</a>-->
-    <!--    </div>-->
+       //daiki
+        <div>
+            <a href="{!!link_to_route ('members.daiki')}"><img src="pictures/daiki.jpg" alt="ダイキ">daiki</a>
+        </div>
        
-    <!--   //hisashi-->
-    <!--    <div>-->
-    <!--        <a href="{{route ('members.hisashi')}}"><img src="hisashi.jpg" alt="ヒサシ">hisashi</a>-->
-    <!--    </div>-->
-    <!--</div>    -->
+       //hisashi
+        <div>
+            <a href="{!!link_to_route ('members.hisashi')}"><img src="pictures/shisashi.jpg" alt="ヒサシ">hisashi</a>
+        </div>
+    </div>    
 @endsection
