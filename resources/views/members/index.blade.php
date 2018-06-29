bf71f3ac39ee6b772649b37412aa2a89024630bb
daiki.ishikawa <diking@gmail.com>
2018-06-29 05:56:33 +0000

    index img fix2

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -3,32 +3,32 @@
 @section('content')
         //anna
         <div>
-            <a href="{{route ('members.anna')}}" img src="annie.jpg" alt="アンナ">anna</a>
+            <a href="{{route ('members.anna')}}"><img src="annie.jpg" alt="アンナ">anna</a>
         </div>
        
        //kanna
         <div>
-            <a href="{{route ('members.kanna')}}" img src="kanna.jpg" alt="カンナ">kanna</a>
+            <a href="{{route ('members.kanna')}}"><img src="kanna.jpg" alt="カンナ">kanna</a>
         </div>
        
        //shingo
         <div>
-            <a href="{{route ('members.shingo')}}" img src="shingo.jpg" alt="シンゴ">shingo</a>
+            <a href="{{route ('members.shingo')}}"><img src="shingo.jpg" alt="シンゴ">shingo</a>
         </div>
        
        //shungo
         <div>
-            <a href="{{route ('members.shungo')}}" img src="shungo.jpg" alt="シュンゴ">shungo</a>
+            <a href="{{route ('members.shungo')}}"><img src="shungo.jpg" alt="シュンゴ">shungo</a>
         </div>
        
        //daiki
         <div>
-            <a href="{{route ('members.daiki')}}" img src="daiki.jpg" alt="ダイキ">daiki</a>
+            <a href="{{route ('members.daiki')}}"><img src="daiki.jpg" alt="ダイキ">daiki</a>
         </div>
        
        //hisashi
         <div>
-            <a href="{{route ('members.hisashi')}}" img src="hisashi.jpg" alt="ヒサシ">hisashi</a>
+            <a href="{{route ('members.hisashi')}}"><img src="hisashi.jpg" alt="ヒサシ">hisashi</a>
         </div>
     </div>    
 @endsection
